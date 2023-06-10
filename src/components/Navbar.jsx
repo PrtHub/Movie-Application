d903509b92d58feb2f3e6b1df5d92e236e1e93bb
Pritam <pri10145@gmail.com>
2023-06-10 09:43:01 +0530

    icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import {  MdFavoriteBorder, MdOutlineSearch } from 'react-icons/md'
+import { BiMoviePlay  } from 'react-icons/bi'
 
 
 const Navbar = () => {
@@ -7,8 +8,9 @@ const Navbar = () => {
     <nav className="w-full h-full bg-pink flex justify-between items-center gap-10 py-5">
       <main className='w-full max-w-7xl mx-auto flex items-center justify-between gap-10 px-20'>
      <section className="flex items-center justify-center gap-16">
-       <section className="font-bold text-2xl">
-          Movies Hub
+       <section className="font-bold text-2xl flex items-center gap-2">
+       <BiMoviePlay/>
+          TMDB
        </section>
        <ul className="flex items-center justify-center gap-10 font-semibold text-lg">
         <li>Movies</li>
