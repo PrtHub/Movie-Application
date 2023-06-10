41e09d63396842b388149f60a9abf9e719bd8892
Pritam <pri10145@gmail.com>
2023-06-10 08:30:29 +0530

    basic structure set

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
+import {  MdFavoriteBorder, MdOutlineSearch } from 'react-icons/md'
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className="w-full h-full bg-pink flex justify-between items-center gap-10 py-5">
+      <main className='w-full max-w-7xl mx-auto flex items-center justify-between gap-10 px-20'>
+     <section className="flex items-center justify-center gap-16">
+       <section className="font-bold text-2xl">
+          Movies Hub
+       </section>
+       <ul className="flex items-center justify-center gap-10 font-semibold text-lg">
+        <li>Movies</li>
+        <li>Tv</li>
+        <li>People</li>
+       </ul>
+     </section>
+     <section className="flex items-center justify-center gap-5">
+        <MdFavoriteBorder className='w-6 h-6 font-semibold cursor-pointer'/>
+        <MdOutlineSearch className='w-6 h-6 font-semibold cursor-pointer'/>
+     </section>
+      </main>
+    </nav>
+    </>
   )
 }
 
