3faa35c5865d09c6a6b3d2911541ec0a9e675ed0
Pritam <pri10145@gmail.com>
2023-06-13 20:51:03 +0530

    updated styles

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -6,7 +6,7 @@ import LazyImg from "./LazyImg";
 import { BsPlayCircle } from "react-icons/bs";
 import { MdFavorite } from "react-icons/md";
 
-const DetailsHeader = ({ details, platforms, crew }) => {
+const DetailsHeader = ({ details, platforms, crew, loading }) => {
   // date
   const formatDate = (dateStr) => {
     const options = { month: "short", day: "2-digit", year: "numeric" };
@@ -34,10 +34,10 @@ const DetailsHeader = ({ details, platforms, crew }) => {
   const revenueInMillion = (details.revenue / 1000000).toFixed(2);
 
   //watch providers
-  const flatrate = platforms?.flatrate.map(
+  const flatrate = platforms?.flatrate?.map(
     (platform) => platform.provider_name
   );
-  const buy = platforms?.buy.map((platform) => platform.provider_name);
+  const buy = platforms?.buy?.map((platform) => platform.provider_name);
 
   //crew members
   const director = crew?.filter(
@@ -53,125 +53,140 @@ const DetailsHeader = ({ details, platforms, crew }) => {
   const writerName = [...new Set(writer?.map((w) => w.name))];
   const uniqueWritersNames = [...new Set(writerName)];
 
+  if (loading) return "loading";
+
   return (
     <>
-      <ContentWrapper>
-        <main className="w-full h-full flex justify-center items-start gap-10 p-10 absolute top-0 right-0 left-0 z-50 opacity-100">
-          <section className="w-96 h-[480px]">
-            <LazyImg
-              src={`https://image.tmdb.org/t/p/original${details?.poster_path}`}
-              className="w-full h-full object-cover object-center rounded"
-            />
-          </section>
-          <section className="w-full h-full flex flex-col items-start justify-start gap-5">
-            <section className="flex flex-col">
-              <h1 className="text-4xl font-bold capitalize">
-                {details?.title}{" "}
-                <span className="font-medium">
-                  ({formatYear(details?.release_date)})
-                </span>
-              </h1>
-              <ul className="flex items-center gap-2">
-                <li className="font-medium text-base">
-                  {formatDate(details?.release_date)}
-                </li>
-                <li className="font-medium text-base list-disc ml-4">
-                  {genres.join(", ")}
-                </li>
-                <li className="font-medium text-base list-disc ml-4">
-                  {formatTime(details?.runtime)} ({details.original_language})
-                </li>
-              </ul>
+      <section className="w-full h-[1200px] lg:h-[600px] relative">
+        <div className="w-full h-full bg-[#00008080] bg-opacity-80 absolute z-0" />
+        <div className="w-full h-full">
+          <img
+            src={`https://image.tmdb.org/t/p/original${details?.backdrop_path}`}
+            alt="backdrop_img"
+            className="w-full h-full object-cover object-center  "
+          />
+        </div>
+        <div className="lower-layer -mb-2"></div>
+        <ContentWrapper>
+          <main className="w-full max-w-7xl mx-auto h-full flex flex-col lg:flex-row justify-center items-start gap-10 p-10 absolute top-0 z-40 opacity-100">
+            <section className="w-[400px] h-[550px] lg:h-[480px]">
+              <LazyImg
+                src={`https://image.tmdb.org/t/p/original${details?.poster_path}`}
+                className="w-full h-full object-cover object-center rounded"
+              />
             </section>
-            <section className=" flex items-center gap-5 ">
-              <section className="flex items-center gap-1">
-                <div className="w-12 h-12 rounded-[50%] p-[3px] bg-[#081c22] font-semibold">
-                  <CircularProgressbar
-                    value={details.vote_average.toFixed(1)}
-                    maxValue={10}
-                    text={details.vote_average.toFixed(1)}
-                    styles={buildStyles({
-                      pathColor:
-                        details.vote_average < 5
-                          ? "red"
-                          : details.vote_average < 7
-                          ? "orange"
-                          : "green",
-                      textColor: "white",
-                      textSize: "38px",
-                    })}
-                  />
-                </div>
-                <span className="font-semibold text-lg">User Rating</span>
+            <section className="w-full h-full flex flex-col items-start justify-start gap-5 mr-5 lg:mr-0">
+              <section className="flex flex-col">
+                <h1 className="text-4xl font-bold capitalize">
+                  {details?.title}{" "}
+                  <span className="font-medium">
+                    ({formatYear(details?.release_date)})
+                  </span>
+                </h1>
+                <ul className="flex items-center gap-2">
+                  <li className="font-medium text-base">
+                    {formatDate(details?.release_date)}
+                  </li>
+                  <li className="font-medium text-base list-disc ml-4">
+                    {genres.join(", ")}
+                  </li>
+                  <li className="font-medium text-base list-disc ml-4">
+                    {formatTime(details?.runtime)} ({details.original_language})
+                  </li>
+                </ul>
               </section>
+              <section className=" flex items-center gap-5 ">
+                <section className="flex items-center gap-1">
+                  <div className="w-12 h-12 rounded-[50%] p-[3px] bg-[#081c22] font-semibold">
+                    <CircularProgressbar
+                      value={details.vote_average.toFixed(1)}
+                      maxValue={10}
+                      text={details.vote_average.toFixed(1)}
+                      styles={buildStyles({
+                        pathColor:
+                          details.vote_average < 5
+                            ? "red"
+                            : details.vote_average < 7
+                            ? "orange"
+                            : "green",
+                        textColor: "white",
+                        textSize: "38px",
+                      })}
+                    />
+                  </div>
+                  <span className="font-semibold text-lg">User Rating</span>
+                </section>
 
-              <span className="flex items-center gap-1 font-medium text-lg">
-                <MdFavorite className="bg-[#081c22] text-4xl p-2 rounded-full cursor-pointer " />
-                Favorite
-              </span>
-              <span className="flex items-center gap-1 font-semibold text-lg cursor-pointer">
-                <BsPlayCircle className="text-4xl" />
-                Play Trailer
-              </span>
-            </section>
-            <article className="flex flex-col items-start gap-4">
-              <p className="text-base italic font-medium">{details.tagline}</p>
-              <div className="flex flex-col gap-2">
-                <h1 className="text-2xl font-semibold">Overview</h1>
-                <p className="text-base font-medium">{details.overview}</p>
-              </div>
-            </article>
-            <section className="flex items-center  gap-5">
-              <p className="text-xl font-medium">
-                Budget:{" "}
-                <span className="text-base ml-1">
-                  {budgetInMillion} million USD
+                <span className="flex items-center gap-1 font-medium text-lg">
+                  <MdFavorite className="bg-[#081c22] text-4xl p-2 rounded-full cursor-pointer " />
+                  Favorite
                 </span>
-              </p>
-              <p className="text-xl font-medium">
-                Revenue:{" "}
-                <span className="text-base ml-1">
-                  {revenueInMillion} million USD
+                <span className="flex items-center gap-1 font-semibold text-lg cursor-pointer">
+                  <BsPlayCircle className="text-4xl" />
+                  Play Trailer
                 </span>
-              </p>
+              </section>
+              <article className="flex flex-col items-start gap-4">
+                <p className="text-base italic font-medium">
+                  {details.tagline}
+                </p>
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-2xl font-semibold">Overview</h1>
+                  <p className="text-base font-medium">{details.overview}</p>
+                </div>
+              </article>
+              <section className="flex items-center  gap-5">
+                <p className="text-xl font-medium">
+                  Budget:{" "}
+                  <span className="text-base ml-1">
+                    {budgetInMillion} million USD
+                  </span>
+                </p>
+                <p className="text-xl font-medium">
+                  Revenue:{" "}
+                  <span className="text-base ml-1">
+                    {revenueInMillion} million USD
+                  </span>
+                </p>
+              </section>
+              {flatrate || buy ? (
+                <section className="w-full">
+                  <p className="text-xl font-medium">
+                    Watch on:{" "}
+                    <span className="text-base font-normal ml-1">
+                      {flatrate && `${flatrate}, `}
+                      {buy && buy.join(", ")}
+                    </span>
+                  </p>
+                  <hr />
+                </section>
+              ) : null}
+              {uniqueDirectorNames.length > 0 && (
+                <section className="w-full">
+                  <p className="text-xl font-medium">
+                    Director:{" "}
+                    <span className="text-base font-normal ml-1">
+                      {uniqueDirectorNames.splice(0, 3).join(", ")}
+                    </span>
+                  </p>
+                  <hr />
+                </section>
+              )}
+              {uniqueWritersNames.length > 0 && (
+                <section className="w-full">
+                  <p className="text-xl font-medium">
+                    Story & Screen play:{" "}
+                    <span className="text-base font-normal ml-1">
+                      {uniqueWritersNames.splice(0, 3).join(", ")}
+                    </span>
+                  </p>
+                  <hr />
+                </section>
+              )}
             </section>
-            {flatrate || buy ? (
-              <section className="w-full">
-                <p className="text-xl font-medium">
-                  Watch on:{" "}
-                  <span className="text-base font-normal ml-1">
-                    {flatrate && `${flatrate}, `}
-                    {buy && buy.join(", ")}
-                  </span>
-                </p>
-                <hr />
-              </section>
-            ) : null}
-            {uniqueDirectorNames.length > 0 && (
-              <section className="w-full">
-                <p className="text-xl font-medium">
-                  Director:{" "}
-                  <span className="text-base font-normal ml-1">
-                    {uniqueDirectorNames.splice(0, 3).join(", ")}
-                  </span>
-                </p>
-                <hr />
-              </section>
-            )}
-            {uniqueWritersNames.length > 0 && (
-              <section className="w-full">
-                <p className="text-xl font-medium">
-                  Story & Screen play:{" "}
-                  <span className="text-base font-normal ml-1">
-                    {uniqueWritersNames.splice(0, 3).join(", ")}
-                  </span>
-                </p>
-                <hr />
-              </section>
-            )}
-          </section>
-        </main>
-      </ContentWrapper>
+          </main>
+        </ContentWrapper>
+      </section>
     </>
   );
 };
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,33 +1,69 @@
-import { DetailsHeader, LazyImg } from "../components";
-import { useGetMovieCraditsQuery, useGetMovieDetailsQuery, useGetWatchMovieQuery } from "../redux/TMDB";
+import ContentWrapper from "../Hoc/SectionWrapper";
+import { DetailsHeader } from "../components";
+import {
+  useGetMovieCraditsQuery,
+  useGetMovieDetailsQuery,
+  useGetWatchMovieQuery,
+} from "../redux/TMDB";
 import { useParams } from "react-router-dom";
 
-
 const MovieDetails = () => {
-  const {movie_id } = useParams();
-  const { data: details, isFetching, error } = useGetMovieDetailsQuery(movie_id)
-  const { data: platforms} = useGetWatchMovieQuery(movie_id)
-  const { data: credits} = useGetMovieCraditsQuery(movie_id)
+  const { movie_id } = useParams();
+  const {
+    data: details,
+    isFetching,
+    error,
+  } = useGetMovieDetailsQuery(movie_id);
+  const { data: platforms } = useGetWatchMovieQuery(movie_id);
+  const { data: credits } = useGetMovieCraditsQuery(movie_id);
 
-  const backdrop_path = details?.backdrop_path
-  
-  
+  const skeleton = () => {
+    return (
+      <ContentWrapper>
+        <section className="w-full h-[600px] relative">
+          <section className="w-full h-full flex justify-center items-start gap-10 p-10 absolute top-0 right-0 left-0 z-50 opacity-100 animate-pulse">
+            <section className="w-[400px] h-[480px] bg-skeleton animate-pulse">
+              <div className="w-full h-full object-cover object-center rounded " />
+            </section>
+            <section className="w-full h-full flex flex-col items-start justify-start gap- animate-pulse">
+              <section className="w-[80%] full flex flex-col animate-pulse gap-5">
+                <div className="w-[80%] h-6 bg-skeleton" />
+                <div className="w-[70%] h-6 bg-skeleton"></div>
+              </section>
+              <section className="w-full h-full flex flex-col items-start gap-5 animate-pulse">
+                <div className="w-[40%] h-6 bg-skeleton" />
+                <div className="w-[50%] h-6 bg-skeleton" />
+                <div className="w-full h-8 bg-skeleton" />
+              </section>
+              <section className="w-[70%] h- flex flex-col gap-5  animate-pulse">
+                <div className="w-[50%] h-6 bg-skeleton" />
+                <div className="w-[60%] h-6 bg-skeleton" />
+                <div className="w-[65%] h-6 bg-skeleton" />
+              </section>
+            </section>
+          </section>
+        </section>
+      </ContentWrapper>
+    );
+  };
+
   return (
     <>
-    {details && (
-     <div className="w-full h-full mt-10 mb-20">
-      <section className="w-full h-[600px] relative">
-      <div className="w-full h-full bg-[#00008080] bg-opacity-80 absolute z-0" />
-        <div className="w-full h-full">
-       <img src={`https://image.tmdb.org/t/p/original${backdrop_path}`} 
-       alt="backdrop_img" 
-       className="w-full h-full object-cover object-center  "/>
-        </div>
-       <div className="lower-layer -mb-2"></div>
-       <DetailsHeader details={details} platforms={platforms?.results?.IN} crew={credits?.crew}/>
-      </section>
-     </div> 
-    )}   
+      {!isFetching ? (
+        <>
+          {details && (
+            <div className="w-full h-full mt-10 mb-20">
+              <DetailsHeader
+                details={details}
+                platforms={platforms?.results?.IN}
+                crew={credits?.crew}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="">{skeleton()}</div>
+      )}
     </>
   );
 };
