f42d5081301e0c684a9ed99e80ea0ce919bcdac0
Pritam <pri10145@gmail.com>
2023-06-14 22:05:01 +0530

    know for movies created

--- src/container/Details/KnownFor.jsx
+++ src/container/Details/KnownFor.jsx
@@ -1,9 +1,85 @@
+/* eslint-disable react/prop-types */
+import { LazyImg } from "../../components";
+import poster from "../../assets/poster.jpg";
+import { useNavigate } from "react-router-dom";
 
+const KnownFor = ({ movie, isLoading }) => {
+  const navigate = useNavigate();
+  const formatDate = (dateStr) => {
+    const options = { month: "short", day: "2-digit", year: "numeric" };
+    const date = new Date(dateStr);
+    return date.toLocaleDateString("en-US", options);
+  };
 
-const KnownFor = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+    if (movie) {
+      navigate(`/movie/${movie.id}`);
+    } else {
+      error("No route available");
+    }
+  };
+
+  const skeleton = () => {
+    return (
+      <main className="w-36 h-56 animate-pulse flex flex-col items-center justify-center gap-2">
+      <div className="w-full h-full bg-skeleton rounded "/>
+      <section className="w-full flex flex-col gap-2 animate-pulse">
+       <div className="w-full h-4 bg-skeleton rounded"/>
+       <div className="w-[80%] h-4 bg-skeleton rounded"/>
+      </section>
+    </main>
+    );
+  };
+
   return (
-    <div>KnownFor</div>
-  )
-}
+    <>
+      {!isLoading ? (
+        <div className="w-36 h-full flex flex-col items-start justify-start gap-1">
+          <section
+            className="w-full h-56 cursor-pointer overflow-hidden rounded"
+            onClick={handleClick}
+          >
+            <LazyImg
+              src={
+                `https://image.tmdb.org/t/p/original${movie.poster_path}` ||
+                poster
+              }
+              alt="Poster"
+              className="w-full h-full object-contain object-center rounded"
+            />
+          </section>
+          <section className="flex flex-col items-start justify-start gap-1">
+            <h1 className="font-semibold ">{movie.title}</h1>
+            <span className="text-gray-400">
+              {formatDate(movie.release_date)}
+            </span>
+          </section>
+        </div>
+      ) : (
+        <div className="flex overflow-y-hidden px-5 gap-5">
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default KnownFor
+export default KnownFor;
--- src/pages/PeopleDetails.jsx
+++ src/pages/PeopleDetails.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
-import { useGetKnownForDetailsQuery, useGetPeopleDetailsQuery } from "../redux/TMDB";
+import {
+  useGetKnownForDetailsQuery,
+  useGetPeopleDetailsQuery,
+} from "../redux/TMDB";
 import ContentWrapper from "../Hoc/SectionWrapper";
 import { LazyImg } from "../components";
 import { useState } from "react";
@@ -8,9 +11,8 @@ import { KnownFor } from "../container";
 const PeopleDetails = () => {
   const { person_id } = useParams();
   const { data: people, isFetching } = useGetPeopleDetailsQuery(person_id);
-  const { data: movies, isLoading} = useGetKnownForDetailsQuery(person_id);
-  console.log(movies?.cast)
-
+  const { data: movies, isLoading } = useGetKnownForDetailsQuery(person_id);
+  console.log(movies?.cast);
 
   const [show, setShow] = useState(false);
   const biography = show
@@ -19,55 +21,64 @@ const PeopleDetails = () => {
 
   return (
     <>
-      {!isFetching  ? (
+      {!isFetching ? (
         <ContentWrapper>
           <div className="w-full h-full max-w-7xl mx-auto mt-10 mb-20">
-          <main className="w-full h-full flex flex-col lg:flex-row justify-center items-start gap-10 py-10 z-40  mt-10 mb-20">
-            <section className="w-[320px] sm:w-[400px] h-[450px] sm:h-[550px] lg:h-[480px]">
-              <LazyImg
-                src={`https://image.tmdb.org/t/p/original${people && people?.profile_path}`}
-                className="w-full h-full object-cover object-center rounded"
-              />
-            </section>
-            <section className="w-[80%] h-full flex flex-col items-start justify-start gap-5 mr-5 lg:mr-0">
-              <section className="w-full h-full flex flex-col gap-1">
-                <h1 className="text-5xl font-bold capitalize">
-                  {people && people?.name}
+            <main className="w-full h-full flex flex-col lg:flex-row justify-center items-start gap-10 py-10 z-40  mt-10 mb-20">
+              <section className="w-[320px] sm:w-[400px] h-[450px] sm:h-[550px] lg:h-[480px]">
+                <LazyImg
+                  src={`https://image.tmdb.org/t/p/original${
+                    people && people?.profile_path
+                  }`}
+                  className="w-full h-full object-cover object-center rounded"
+                />
+              </section>
+              <section className="w-[80%] h-full flex flex-col items-start justify-start gap-5 mr-5 lg:mr-0">
+                <section className="w-full h-full flex flex-col gap-1">
+                  <h1 className="text-5xl font-bold capitalize">
+                    {people && people?.name}
+                  </h1>
+                  <p className="font-medium text-lg ">
+                    {people && people?.known_for_department}
+                  </p>
+                </section>
+                <article className="w-full flex flex-col items-start gap-2 pr-10">
+                  <h1 className="text-3xl font-semibold">Biography</h1>
+                  <p className="w-full max-w-7xl text-base font-normal ">
+                    {people && biography}
+                  </p>
+                  {!show ? (
+                    <span
+                      className="cursor-pointer text-lg font-semibold"
+                      onClick={() => setShow(true)}
+                    >
+                      Show more
+                    </span>
+                  ) : (
+                    <span
+                      className="cursor-pointer text-lg font-semibold"
+                      onClick={() => setShow(false)}
+                    >
+                      Show less
+                    </span>
+                  )}
+                </article>
+              </section>
+            </main>
+            <div className="w-full h-full flex flex-col gap-10">
+              <section className="w-full flex items-center gap-10">
+                <h1 className="text-2xl sm:text-3xl font-semibold">
+                  Known For
                 </h1>
-                <p className="font-medium text-lg ">
-                  { people && people?.known_for_department}
-                </p>
               </section>
-              <article className="w-full flex flex-col items-start gap-2 pr-10">
-                <h1 className="text-3xl font-semibold">Biography</h1>
-                <p className="w-full max-w-7xl text-base font-normal ">
-                  { people && biography}
-                </p>
-                {!show ? (
-                  <span
-                    className="cursor-pointer text-lg font-semibold"
-                    onClick={() => setShow(true)}
-                  >
-                    Show more
-                  </span>
-                ) : (
-                  <span
-                    className="cursor-pointer text-lg font-semibold"
-                    onClick={() => setShow(false)}
-                  >
-                    Show less
-                  </span>
-                )}
-              </article>
-            </section>
-          </main>
-         <section className="flex flex-row gap-5 overflow-x-scroll">
-          {movies?.cast?.map((movie) => (
-            <section key={movie.id}>
-               <KnownFor movie={movie} isLoading={isLoading}/>
+              <section className="flex flex-row gap-5 overflow-x-scroll">
+                {movies?.cast?.map((movie) => (
+                  <section key={movie.id}>
+                    <KnownFor movie={movie} isLoading={isLoading} />
+                  </section>
+                ))}
               </section>
-            ))}
-            </section>
+            </div>
           </div>
         </ContentWrapper>
       ) : (
