1e043832aceb8e8a1300effe41ff63611dd47751
Pritam <pri10145@gmail.com>
2023-06-15 19:59:05 +0530

    add skeleton

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -6,17 +6,29 @@ import { useGetPopularPeopleQuery } from "../redux/TMDB";
 
 const People = () => {
   const [pageNum, setPageNum] = useState(1);
-  const [allShows, setAllShows] = useState([]);
+  const [allPeople, setAllPeople] = useState([]);
   const { data: people, isFetching, error } = useGetPopularPeopleQuery(pageNum);
 
   useEffect(() => {
     if (people?.results) {
-      setAllShows((prevShows) => [...prevShows, ...people.results]);
+      setAllPeople((prevShows) => [...prevShows, ...people.results]);
     }
   }, [people]);
 
   const fetchNextPage = () => {
     setPageNum((prevPageNum) => prevPageNum + 1);
+  };
+
+  const skeleton = () => {
+    return (
+      <main className="w-40 h-60  animate-pulse flex flex-col items-center justify-center gap-2">
+        <div className="w-full h-full bg-skeleton rounded " />
+        <section className="w-full flex flex-col gap-2 ">
+          <div className="w-full h-4 bg-skeleton rounded" />
+          <div className="w-[80%] h-4 bg-skeleton rounded" />
+        </section>
+      </main>
+    );
   };
 
   if (error) return <Error />;
@@ -25,18 +37,36 @@ const People = () => {
     <div className="w-full h-full py-10">
       <ContentWrapper>
         <InfiniteScroll
-          dataLength={allShows.length}
+          dataLength={allPeople.length}
           next={fetchNextPage}
           hasMore={pageNum <= (people?.total_pages || 0)}
           loader={<Loader />}
         >
-          <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
-            {allShows.map((person, index) => (
-              <section key={`${person.id}-${index}`}>
-                <PeopleCard person={person} isFetching={isFetching} />
-              </section>
-            ))}
-          </div>
+          {!isFetching ? (
+            <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
+              {allPeople.map((person, index) => (
+                <section key={`${person.id}-${index}`}>
+                  <PeopleCard person={person} />
+                </section>
+              ))}
+            </div>
+          ) : (
+            <div className="w-full h-full flex flex-wrap justify-center overflow-x-hidden px-5 gap-5">
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+            </div>
+          )}
         </InfiniteScroll>
       </ContentWrapper>
     </div>
