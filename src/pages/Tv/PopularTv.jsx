4ca9ca0a8811dd9d2bf592dec28d82cae2135a5c
Pritam <pri10145@gmail.com>
2023-06-15 19:48:29 +0530

    skeleton added in tv pages

--- src/pages/Tv/PopularTv.jsx
+++ src/pages/Tv/PopularTv.jsx
@@ -45,6 +45,18 @@ const PopularTV = () => {
     }
   }, [genre]);
 
+  const skeleton = () => {
+    return (
+      <main className="w-40 h-60  animate-pulse flex flex-col items-center justify-center gap-2">
+        <div className="w-full h-full bg-skeleton rounded " />
+        <section className="w-full flex flex-col gap-2 ">
+          <div className="w-full h-4 bg-skeleton rounded" />
+          <div className="w-[80%] h-4 bg-skeleton rounded" />
+        </section>
+      </main>
+    );
+  };
+
   if (error) return <Error />;
 
   return (
@@ -71,13 +83,31 @@ const PopularTV = () => {
           hasMore={pageNum <= (shows?.total_pages || 0)}
           loader={<Loader />}
         >
-          <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
-            {allShows.map((media, index) => (
-              <section key={`${media.id}-${index}`}>
-                <MovieCard Media={media} isFetching={isFetching} />
-              </section>
-            ))}
-          </div>
+          {!isFetching ? (
+            <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
+              {allShows.map((media, index) => (
+                <section key={`${media.id}-${index}`}>
+                  <MovieCard Media={media} />
+                </section>
+              ))}
+            </div>
+          ) : (
+            <div className="w-full h-full flex flex-wrap justify-center overflow-x-hidden px-5 gap-5">
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+              {skeleton()}
+            </div>
+          )}
         </InfiniteScroll>
       </ContentWrapper>
     </div>
