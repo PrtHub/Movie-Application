f1813473df79ead34c59d704b3755fa41efbddb1
Pritam <pri10145@gmail.com>
2023-06-16 09:48:31 +0530

    search page completd

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,9 +1,49 @@
+/* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
+import LazyImg from "./LazyImg";
+import dummy from "../assets/poster.jpg";
 
+const SearchCard = ({ result }) => {
+  const navigate = useNavigate();
 
-const SearchCard = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    scroll.scrollToTop({
+      duration: 500,
+      smooth: true,
+    });
+    if (result) {
+      navigate(`/${result.media_type}/${result.id}`);
+    } else {
+      alert("No route available");
+    }
+  };
+
+  const imgUrl =
+    result.poster_path || result.profile_path || result.backdrop_path
+      ? `https://image.tmdb.org/t/p/original${
+          result.poster_path || result.profile_path || result.backdrop_path
+        }`
+      : dummy;
+
   return (
-    <div>SearchCard</div>
-  )
-}
+    <div className="w-36 h-full flex flex-col items-start justify-start gap-1">
+      <section
+        className="w-full h-56 cursor-pointer overflow-hidden rounded"
+        onClick={handleClick}
+      >
+        <LazyImg
+          src={imgUrl}
+          alt="Poster"
+          className="w-full h-full object-cover object-center rounded"
+        />
+      </section>
+      <section className="flex flex-col items-start justify-start">
+        <h1 className="font-semibold ">{result.title || result.name}</h1>
+      </section>
+    </div>
+  );
+};
 
-export default SearchCard
+export default SearchCard;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,50 +1,126 @@
 import { useParams } from "react-router-dom";
 import ContentWrapper from "../Hoc/SectionWrapper";
 import { useGetSearchMultiQuery } from "../redux/TMDB";
-import { SearchCard } from "../components";
+import { Error, Loader, SearchCard } from "../components";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useEffect, useState } from "react";
+import Select from "react-select";
+
+const categories = [
+  { name: "People", media_type: "person" },
+  { name: "Movies", media_type: "movie" },
+  { name: "Tv Shows", media_type: "tv" },
+];
 
 const Search = () => {
   const { query } = useParams();
   const [pageNum, setPageNum] = useState(1);
-  const [allContent, setAllMovies] = useState([])
-  const { data: content, isFetching, error } = useGetSearchMultiQuery({query, pageNum});
-  console.log(content)
+  const [allContent, setAllContent] = useState([]);
+  const [category, setCategory] = useState(null);
+  const {
+    data: content,
+    isFetching,
+    error,
+  } = useGetSearchMultiQuery({ query, pageNum });
+  console.log(content);
 
   useEffect(() => {
-    if(content?.results) {
-      setAllMovies((prevContent) =>[ ...prevContent, ...content.results])
+    if (content?.results) {
+      setAllContent((prevContent) => [...prevContent, ...content.results]);
     }
-  }, [content])
+  }, [content]);
 
-
-  if(isFetching) return "Loading....."
-  if(error) return "Something went wrong"
+  useEffect(() => {
+    if (!category || category.length === 0) {
+      setPageNum(1);
+    }
+  }, [category]);
 
   const fetchNextPageData = () => {
-    setPageNum((prevPage) => prevPage + 1)
+    setPageNum((prevPage) => prevPage + 1);
   };
 
-  
+  const onChange = (selectOptions) => {
+    setCategory(selectOptions);
+
+    const selectCategoriesId = selectOptions.map((option) => option.media_type);
+    if (selectCategoriesId.length === 0) {
+      setAllContent(content?.results);
+    } else {
+      const filteredContent = content?.results.filter((item) =>
+        selectCategoriesId.includes(item.media_type)
+      );
+      setAllContent(filteredContent);
+    }
+  };
+
+  if (error) return <Error />;
+
+  const skeleton = () => {
+    return (
+      <main className="w-40 h-60  animate-pulse flex flex-col items-center justify-center gap-2">
+        <div className="w-full h-full bg-skeleton rounded " />
+        <section className="w-full flex flex-col gap-2 ">
+          <div className="w-full h-4 bg-skeleton rounded" />
+          <div className="w-[80%] h-4 bg-skeleton rounded" />
+        </section>
+      </main>
+    );
+  };
+
   return (
     <>
-    <div className="w-full h-full px-10 py-20">
-      <ContentWrapper>
-        <InfiniteScroll
-         className="w-full h-full flex flex-wrap items-start justify-start gap-5"
-         dataLength={allContent.length}
-         next={fetchNextPageData}
-         hasMore={pageNum <= (content?.total_pages || 0)}
-         >
-        {allContent.map((result) => (
-          <section key={result.id}>
-            <SearchCard />
+      <div className="w-full h-full py-10">
+        <ContentWrapper>
+          <section className="w-full h-full flex items-center justify-end mb-10 px-10">
+            <Select
+              isMulti
+              name="catagory"
+              value={category}
+              onChange={onChange}
+              closeMenuOnSelect={false}
+              options={categories}
+              getOptionLabel={(option) => option.name}
+              getOptionValue={(option) => option.media_type}
+              placeholder="Select category"
+              classNamePrefix="react-select"
+              className="w-full sm:w-[300px] text-black"
+            />
           </section>
-        ))}
-        </InfiniteScroll>
-      </ContentWrapper>
-    </div>
+          <InfiniteScroll
+            dataLength={allContent.length}
+            next={fetchNextPageData}
+            hasMore={pageNum <= (content?.total_pages || 0)}
+            loader={<Loader />}
+          >
+            {!isFetching ? (
+              <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
+                {allContent.map((result, index) => (
+                  <section key={`${result.id}-${index}`}>
+                    <SearchCard result={result} />
+                  </section>
+                ))}
+              </div>
+            ) : (
+              <div className="w-full h-full flex flex-wrap justify-center overflow-x-hidden px-5 gap-5">
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+                {skeleton()}
+              </div>
+            )}
+          </InfiniteScroll>
+        </ContentWrapper>
+      </div>
     </>
   );
 };
