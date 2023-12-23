85b64aeb8a234caa507c9e5dc2f02ef33362f621
Pritam <pri10145@gmail.com>
2023-12-23 09:11:59 +0530

    Paginate

--- src/pages/Movie/NowPlaying.jsx
+++ src/pages/Movie/NowPlaying.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
-import { Error, Loader, MovieCard, SEO } from "../../components";
+import { Error, MovieCard, SEO } from "../../components";
 import ContentWrapper from "../../Hoc/SectionWrapper";
 import {
   useGetGenresDataQuery,
   useGetNowPlaingMovieQuery,
 } from "../../redux/TMDB";
-import InfiniteScroll from "react-infinite-scroll-component";
 import Select from "react-select";
 import MovieSkeleton from "../../components/skeletons/MovieSkeleton";
+import ReactPaginate from "react-paginate";
 
 const NowPlaying = () => {
   const [pageNum, setPageNum] = useState(1);
@@ -21,28 +21,20 @@ const NowPlaying = () => {
   } = useGetNowPlaingMovieQuery(pageNum);
   const { data: genresData } = useGetGenresDataQuery(media_type);
 
+  console.log(genresData)
+
   useEffect(() => {
     if (movies?.results) {
-      setAllMovies((prevMovies) => [...prevMovies, ...movies.results]);
+      setAllMovies(movies.results);
     }
   }, [movies]);
 
-  const fetchNextPage = () => {
-    setPageNum((prevPageNum) => prevPageNum + 1);
-  };
-
   const onChange = (selectedOptions) => {
     setGenre(selectedOptions);
-
-    // Filter movies based on selected genres
     const selectedGenreIds = selectedOptions.map((option) => option.id);
-
-    // Check if any genre is selected
     if (selectedGenreIds.length === 0) {
-      // Show all movies when no genre is selected
       setAllMovies(movies?.results);
     } else {
-      // Filter movies when genres are selected
       const filteredMovies = movies?.results.filter((movie) =>
         movie.genre_ids.some((genreId) => selectedGenreIds.includes(genreId))
       );
@@ -50,44 +42,42 @@ const NowPlaying = () => {
     }
   };
 
-  // Reset pageNum to 1 when genre is cleared
   useEffect(() => {
     if (!genre || genre.length === 0) {
       setPageNum(1);
     }
   }, [genre]);
 
+  const handlePageChange = ({ selected }) => {
+    window.scrollTo(0, 0);
+    setPageNum(selected + 1);
+  };
+
   if (error) return <Error />;
 
   return (
     <>
-    <SEO title="Now Playing Movies - The Movie Database (TMDB)"/>
-    <div className="w-full h-full py-10">
-      <ContentWrapper>
-        <section className="w-full h-full flex items-center justify-end mb-10 px-10">
-          <Select
-            isMulti
-            name="genres"
-            value={genre}
-            onChange={onChange}
-            closeMenuOnSelect={false}
-            options={genresData?.genres}
-            getOptionLabel={(option) => option.name}
-            getOptionValue={(option) => option.id}
-            placeholder="Select genres"
-            classNamePrefix="react-select"
-            className="w-full sm:w-[300px] text-black"
-          />
-        </section>
-        <InfiniteScroll
-          dataLength={allMovies.length}
-          next={fetchNextPage}
-          hasMore={pageNum <= (movies?.total_pages || 0)}
-          loader={<Loader />}
-        >
+      <SEO title="Now Playing Movies - The Movie Database (TMDB)" />
+      <div className="w-full h-full py-10">
+        <ContentWrapper>
+          <section className="w-full h-full flex items-center justify-end mb-10 px-10">
+            <Select
+              isMulti
+              name="genres"
+              value={genre}
+              onChange={onChange}
+              closeMenuOnSelect={false}
+              options={genresData?.genres}
+              getOptionLabel={(option) => option.name}
+              getOptionValue={(option) => option.id}
+              placeholder="Select genres"
+              classNamePrefix="react-select"
+              className="w-full sm:w-[300px] text-black"
+            />
+          </section>
           {!isFetching ? (
             <div className="w-full h-full flex flex-wrap items-start justify-center gap-5">
-              {allMovies.map((media, index) => (
+              {allMovies?.map((media, index) => (
                 <section key={`${media.id}-${index}`}>
                   <MovieCard Media={media} />
                 </section>
@@ -95,26 +85,36 @@ const NowPlaying = () => {
             </div>
           ) : (
             <div className="w-full h-full flex flex-wrap justify-center overflow-x-hidden px-5 gap-5">
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
-              <MovieSkeleton/>
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
+              <MovieSkeleton />
             </div>
           )}
-        </InfiniteScroll>
-      </ContentWrapper>
-    </div>
+          <section className="w-full flex justify-center mt-5">
+            <ReactPaginate
+              previousLabel={"←"}
+              nextLabel={"→"}
+              pageCount={movies?.total_pages}
+              onPageChange={handlePageChange}
+              forcePage={pageNum - 1}
+              containerClassName="flex item-center justify-center gap-5 py-10 mt-10 "
+              activeClassName="bg-pink px-2 rounded-full"
+            />
+          </section>
+        </ContentWrapper>
+      </div>
     </>
   );
 };
